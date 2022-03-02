436dbe22b13824eaf45ebd955634789f73945187
Borja <3930245+BorjaLL@users.noreply.github.com>
2022-03-02 08:27:36 +0000

    crkbd folder

---
+++ crkbd/keymaps/borjall/secrets/secrets.c
@@ -0,0 +1,2 @@
+#include "keymap.c"  // replace with your keymap's "h" file, or whatever file stores the keycodes
+
---
+++ crkbd/r2g/rules.mk
@@ -0,0 +1,8 @@
+BOOTLOADER      = atmel-dfu
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+RGBLIGHT_ENABLE = yes
+
+SPLIT_KEYBOARD  = yes
+#comment test
